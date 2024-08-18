0a3d8e4a9300033802b2c93e312dcd3478c1b0a0
Emir Kaan Özdemir <155099794+emirkaanozdemr@users.noreply.github.com>
2024-08-18 15:01:22 +0300

    HOW TO OPERATE?

---
+++ HOW TO OPERATE.sh
@@ -0,0 +1,7 @@
+#First be sure that, app.py is in right folder. If you are not, you can operate one line below.
+#cd income_predictor
+
+#To operate
+python app.py
+
+#You can see your application from this link: http://127.0.0.1:5000/
